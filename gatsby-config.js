c9b29b520660ded2056e4c5e4bb8a6fb87cf9af5
Pierre Canthelou <pierrecanthelou@iMac-de-Pierre.local>
2018-08-25 01:38:36 +0200

    Images responsives

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,17 @@ module.exports = {
     description: 'Pierre Canthelou est lead developer chez Bee2Link et spécialiste frontend',
     keywords: 'Pierre Canthelou est lead developer chez Bee2Link et spécialiste frontend'
   },
-  plugins: ['gatsby-plugin-react-helmet','gatsby-plugin-sass',`gatsby-transformer-sharp`,
-  `gatsby-plugin-sharp`],
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `img`,
+        path: `${__dirname}/src/img/`
+      }
+    },
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-sass',
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp'
+  ],
 }
